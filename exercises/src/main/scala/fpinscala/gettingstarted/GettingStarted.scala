09136e3e029fdf1721aa4b871ffa6d83b65fe264
Rajesh Pitty <rajesh.pitty@crest.in>
2017-02-12 13:14:00 +0530

    curry, uncurry and compose

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -21,7 +21,7 @@ object MyModule {
     @annotation.tailrec
     def go(n: Int, acc: Int): Int =
       if (n <= 0) acc
-      else go(n-1, n*acc)
+      else go(n - 1, n * acc)
 
     go(n, 1)
   }
@@ -30,7 +30,9 @@ object MyModule {
   def factorial2(n: Int): Int = {
     var acc = 1
     var i = n
-    while (i > 0) { acc *= i; i -= 1 }
+    while (i > 0) {
+      acc *= i; i -= 1
+    }
     acc
   }
 
@@ -39,12 +41,12 @@ object MyModule {
   def fib(n: Int): Int = {
 
     @annotation.tailrec
-    def go(n: Int, previous:Int, current: Int): Int = {
+    def go(n: Int, previous: Int, current: Int): Int = {
       if (n == 0) previous
-      else go(n-1, current, previous + current)
+      else go(n - 1, current, previous + current)
     }
 
-    go(n,0, 1)
+    go(n, 0, 1)
 
   }
 
@@ -89,6 +91,7 @@ object TestFib {
 // convenient to have syntax for constructing a function
 // *without* having to give it a name
 object AnonymousFunctions {
+
   import MyModule._
 
   // Some examples of anonymous functions:
@@ -99,7 +102,9 @@ object AnonymousFunctions {
     println(formatResult("increment2", 7, (x) => x + 1))
     println(formatResult("increment3", 7, x => x + 1))
     println(formatResult("increment4", 7, _ + 1))
-    println(formatResult("increment5", 7, x => { val r = x + 1; r }))
+    println(formatResult("increment5", 7, x => {
+      val r = x + 1; r
+    }))
   }
 }
 
@@ -117,12 +122,13 @@ object MonomorphicBinarySearch {
       else {
         val mid2 = (low + high) / 2
         val d = ds(mid2) // We index into an array using the same
-                         // syntax as function application
+        // syntax as function application
         if (d == key) mid2
-        else if (d > key) go(low, mid2, mid2-1)
+        else if (d > key) go(low, mid2, mid2 - 1)
         else go(mid2 + 1, mid2, high)
       }
     }
+
     go(0, 0, ds.length - 1)
   }
 
@@ -132,7 +138,7 @@ object PolymorphicFunctions {
 
   // Here's a polymorphic version of `binarySearch`, parameterized on
   // a function for testing whether an `A` is greater than another `A`.
-  def binarySearch[A](as: Array[A], key: A, gt: (A,A) => Boolean): Int = {
+  def binarySearch[A](as: Array[A], key: A, gt: (A, A) => Boolean): Int = {
     @annotation.tailrec
     def go(low: Int, mid: Int, high: Int): Int = {
       if (low > high) -mid - 1
@@ -140,8 +146,8 @@ object PolymorphicFunctions {
         val mid2 = (low + high) / 2
         val a = as(mid2)
         val greater = gt(a, key)
-        if (!greater && !gt(key,a)) mid2
-        else if (greater) go(low, mid2, mid2-1)
+        if (!greater && !gt(key, a)) mid2
+        else if (greater) go(low, mid2, mid2 - 1)
         else go(mid2 + 1, mid2, high)
       }
     }
@@ -170,14 +176,18 @@ object PolymorphicFunctions {
 
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
-  def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    //    f.curried
+    a => b => f(a, b)
+  }
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
-  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
+//        Function.uncurried(f)
+    (a, b) => f(a)(b)
+  }
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -191,6 +201,10 @@ object PolymorphicFunctions {
 
   // Exercise 5: Implement `compose`
 
-  def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+  def compose[A, B, C](f: B => C, g: A => B): A => C = {
+//    f.compose(g)
+      a => f(g(a))
+  }
+
+
 }
